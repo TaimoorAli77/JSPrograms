3fd96449e6422d0725bdb699ac47eab5951a4f4b
Taimoor Ali <taimoorali083@gmail.com>
2023-11-19 14:34:41 +0500

    Obj

--- first.js
+++ first.js
@@ -1,19 +1,12 @@
-const fs = require('fs')
-fs.writeFileSync('new.txt','Hello world')
 
-let name = "Tai"
-const age = 30
-let hobby = true
-console.log(name)
-const summerize =  (n,a,h)=>{
-    return( 'My name is '+ n + ' and age : '+age+ ' and hobby is '+hobby)
+const obj = {
+    name: "Taimoor",
+    rol:89,
+    add:function(){
+        console.log("G kia hal ha ", this.name)
+    },
+    addT(){
+        console.log("G kia hal ha ", this.name)
+    }
 }
-console.log(summerize(name,age,hobby))
-
-const add = (a,b)=>a+b
-console.log(add(3,3))
-const as= a=>a+2
-console.log(as(3))
-
-const g = ()=>4
-console.log(g())
+obj.addT()

bb053558021edd00ac4c46bee50162e809c4ead3
Taimoor Ali <taimoorali083@gmail.com>
2023-11-19 15:00:43 +0500

    spread and rest op

--- first.js
+++ first.js
@@ -13,3 +13,29 @@ arr.shift("Programming")
 console.log(arr)
 arr.pop('100')
 console.log(arr)
+
+let copiedArr = arr.slice()  // slice makes copies (immutability)
+console.log("copiedArr : ",copiedArr)
+
+let secondACopied = [...arr]  // copies (immutability) spread operator
+console.log(secondACopied)
+
+let person = {
+    name:"Taimoor",
+    rollno:34,
+    play:true,
+    add(){
+        console.log("ok",this.name)
+    }
+}
+person.add()
+let copyObj = {...person}   // spread op
+console.log("copyObj",copyObj)
+
+
+// Rest operator
+
+const toArr = (...newArr)=>{
+    return newArr
+}
+console.log(toArr(2,3,5,4))

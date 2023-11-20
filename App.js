9ba91069b4d1643e31888b9379a72aad4f0476ef
Taimoor Ali <taimoorali083@gmail.com>
2023-11-20 16:37:56 +0500

    redirect res.statuscode setHeader

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 const http = require('http')
-
+const fs = require('fs')
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     // process.exit()
     let url = req.url
-    // let method= req.method
+    let method= req.method
     if (url === '/') {
         res.write('<html>')
         res.write('<head><title>Enter Message</title></head>')
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>Submit</button></form></body>')
         res.write('</html>')
+        return res.end()
+    }
+    if(url==='/message' && method==='POST'){
+        fs.writeFileSync('message.txt',"Dummy Text")
+        // res.writeHead(302,{})
+        res.statusCode=302
+        res.setHeader('Location','/')
         return res.end()
     }
         res.setHeader('Content-Type', 'text/html')

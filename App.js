d8de0b21ed06a3749a1de84190829eed5cea7a37
Taimoor Ali <taimoorali083@gmail.com>
2023-11-25 11:01:17 +0500

    initial concepts regarding express js

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 const express = require('express')
-const app = express()
-// const routes = require('./routes.js')
-// console.log(routes.someText)
-app.use((req, res, next) => {
-    console.log("middleware ")
-    next()
-})
+const app = express() 
+const path = require('path')
+const bodyParser= require('body-parser')
+const adminRouter = require('./routes/admin.js')
+const shopRouter = require('./routes/shop.js')
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use((req, res, next) => {
-    console.log("middleware Second")
-    res.send("<h1>Welcome Back</h1>")
+app.use("/admin",adminRouter)
+app.use(shopRouter)
+
+
+app.use('/',(req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 
 app.listen(9090)

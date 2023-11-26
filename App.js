c34aaa7a080b2e52de3bfd89a37351c895858cd7
Taimoor Ali <taimoorali083@gmail.com>
2023-11-26 15:29:59 +0500

    ejs addwd

--- App.js
+++ App.js
@@ -4,16 +4,8 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const adminData = require('./routes/admin.js')
 const shopRouter = require('./routes/shop.js')
-const expressHbs = require('express-handlebars');
 
-app.engine('hbs', expressHbs.engine({
-  layoutsDir: 'views/layouts/',
-  defaultLayout: 'main-layout',
-  extname: 'hbs'
-})
-);
-
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }))
